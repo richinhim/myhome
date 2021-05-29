4a2b7813bebea512fcd66c563f1464d5c025622d
richinhim <kwlsy666@naver.com>
2021-05-29 21:55:27 +0900

    #6.thymleaf에서 form 전송하기

--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -1,11 +1,13 @@
 package com.godcoder.myhome.model;
 
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +15,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
     private String title;
     private String content;
 }
