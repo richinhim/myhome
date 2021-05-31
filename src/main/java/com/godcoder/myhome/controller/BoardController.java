876d39728bd59911929667a2e32c93590b294358
richinhim <kwlsy666@naver.com>
2021-05-31 20:59:43 +0900

    8. JPA를 이용한 페이지 처리 및 검색

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
 import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,10 +27,20 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model, @PageableDefault(size = 2)  Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText){
 
-        List<Board> boards = boardRepository.findAll();
+        //Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
+        //Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText,searchText,pageable);
 
+        //boards.getTotalElements();
+
+        int startPage = Math.max(1,boards.getPageable().getPageNumber() - 4) ;
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4) ;
+        //boards.getPageable().getPageNumber();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
